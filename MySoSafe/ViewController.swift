d05c091188f740a6a7412e83100b429814908cd3
Pedro Rolon <pedrito.rolon94@gmail.com>
2020-08-29 12:47:35 -0400

    initial commit

---
+++ MySoSafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySoSafe
+//
+//  Created by Pedro  on 8/29/20.
+//  Copyright © 2020 ballustra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
